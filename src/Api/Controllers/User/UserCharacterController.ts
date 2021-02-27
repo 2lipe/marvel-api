95f6820e8af6b64e9819a16fb6240bef434c6a62
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-27 18:28:24 -0300

    chore: create user characters and comic controllers

--- src/Api/Controllers/User/UserCharacterController.ts
+++ src/Api/Controllers/User/UserCharacterController.ts
@@ -18,4 +18,36 @@ export class UserCharacterController {
       joiResponseError(err, res, next);
     }
   }
+
+  async getFavoriteCharacter(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      await UserSchemas.userSchemas.userIdScheme.validateAsync(req.params);
+
+      const { userId } = req.params;
+
+      const userService = new UserService();
+
+      const result = await userService.getFavoriteCharacter(userId);
+
+      responseTreated(result, res);
+    } catch (err) {
+      joiResponseError(err, res, next);
+    }
+  }
+
+  async removeFavoriteCharacter(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+      await UserSchemas.characterSchemas.removeCharacterScheme.validateAsync(req.body);
+
+      const { characterId, userId } = req.body;
+
+      const userService = new UserService();
+
+      const result = await userService.removeFavoriteCharacter(characterId, userId);
+
+      responseTreated(result, res);
+    } catch (err) {
+      joiResponseError(err, res, next);
+    }
+  }
 }
