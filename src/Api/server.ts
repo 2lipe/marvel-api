317aa98f73a96af459cc9db5286f080f7ab5bc04
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-27 14:29:43 -0300

    chore: implementing marvel fetchs

--- src/Api/server.ts
+++ src/Api/server.ts
@@ -6,7 +6,7 @@ import helmet from 'helmet';
 
 import { DatabaseConnection } from '../Infrastructure/Database';
 import { serverError } from '../Api/Helpers/http-error-helpers';
-import { envConfig } from '../Api/Configs/env-configs';
+import { envConfigs } from '../Api/Configs/env-configs';
 import { routes } from './Routes';
 import { authMiddleware } from './Middlewares/AuthMiddleware';
 import { unless } from './Configs/path-configs';
@@ -50,8 +50,8 @@ export class Server {
   }
 
   public start(): void {
-    this._server.listen(envConfig.connectionPort || this._port, () => {
-      console.info(`This server is listening on http://localhost:${envConfig.connectionPort || this._port}`);
+    this._server.listen(envConfigs.connectionPort || this._port, () => {
+      console.info(`This server is listening on http://localhost:${envConfigs.connectionPort || this._port}`);
     });
   }
 
