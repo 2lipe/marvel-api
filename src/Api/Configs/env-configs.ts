61e24a74ba20fe49eec6d15591baa39baf26055b
Felipe Vieira <felipe.svfx@gmail.com>
2021-02-27 23:26:18 -0300

    fix: api url

--- src/Api/Configs/env-configs.ts
+++ src/Api/Configs/env-configs.ts
@@ -3,13 +3,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const envConfigs = {
-  databaseName: process.env.DATABASE_NAME,
-  databaseType: process.env.DATABASE_TYPE,
-  databaseHost: process.env.DATABASE_HOST,
-  databasePort: process.env.DATABASE_PORT,
-  databaseUsername: process.env.DATABASE_USERNAME,
-  databasePassword: process.env.DATABASE_PASSWORD,
-
   connectionPort: process.env.SERVER_PORT,
 
   jwtKey: process.env.JWT_KEY,
